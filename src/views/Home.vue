5eadfa0bd26c2c1b1a495b09f14d94f1438e888a
Tien Phan <tien@routeique.com>
2020-07-27 18:49:01 -0600

    Add Vue Select.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,30 @@
   <div class="home">
     <b-container>
       <b-row>
+        <b-col md="8" offset-md="2" class="my-4">
+          <v-select :options="['Canada', 'United States']"></v-select>
+        </b-col>
+      </b-row>
+      <b-row>
         <b-col md="3">
-          sdff
+          <b-card
+            header="Thông báo"
+            class="text-left"
+          >
+            <b-card-text>
+              Vì shop không còn quản lý trang rubiksaigon.com nữa,xin quý khách vui lòng vào
+              <span style="font-weight: bold">rubik-saigon.com</span>
+              (<span style="font-weight: bold">có dấu gạch nối</span>).
+              <br>
+              <br>
+              Rubik-saigon.com chỉ có 1 shop duy nhất tại 300/11 Đội Cung, phường 9, quận 11.
+            </b-card-text>
+          </b-card>
         </b-col>
         <b-col md="9">
           <b-row>
             <b-col lg="4" sm="6" xs="12" v-for="puzzle in puzzles" :key="puzzle.id">
-              <b-card no-body @click="cardClick(puzzle.url)">
+              <b-card no-body @click="cardClick(puzzle.url)" class="mb-5">
                 <b-img :src="puzzle.thumbnail ? `http://localhost:3000${puzzle.thumbnail}` : null" fluid></b-img>
                 <b-card-body>
                   <b-card-title>{{ puzzle.name }}</b-card-title>
@@ -28,8 +45,15 @@
 </template>
 
 <script>
+import vSelect from 'vue-select';
+
+import 'vue-select/dist/vue-select.css';
+
 export default {
   name: 'Home',
+  components: {
+    'v-select': vSelect,
+  },
   data() {
     return {
       puzzles: [],
